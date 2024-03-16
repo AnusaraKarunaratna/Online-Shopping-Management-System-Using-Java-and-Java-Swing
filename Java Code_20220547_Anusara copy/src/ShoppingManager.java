1e5669c90bd4f21aea4ce901617e1cbdfffae8da
anusara2022 <anusara.20220547@iit.ac.lk>
2024-03-17 00:12:21 +0530

    Uploaded the full and complete code files of Shop Management System

--- Java Code_20220547_Anusara copy/src/ShoppingManager.java
+++
@@ -1,8 +0,0 @@
-public interface ShoppingManager {
-
-    void addProducts(Product selectedProduct);
-    void removeProducts(String productID);
-    void saveToAFle();
-    void loadFile();
-    void printData();
-}
